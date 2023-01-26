30e3ba8d6d091c67ffc085f0381bcbde7e79387d
wdd130cassell <cas22015@byui.edu>
2023-01-26 09:41:25 +0000

    add

---
+++ week04/Journal.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.consoel;
+
+namespace JournalApp
+{
+    class Journal
+    {
+        // Display the Introduction, Show menu to select from the options.
+        public void Run()
+        {
+            Writeline("Journal App");
+            ReadKey(true);
+        }
+
+        private void CreateJournalFile() { }
+
+        private void DisplayIntro() { }
+
+        private void DisplayOutro() { }
+
+        private void DisplayJournalContents() { }
+
+        private void AddEntry() { }
+
+        private void ClearFile() { }
+
+    }
+}
---
+++ week04/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace JournalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            consoel.WriteLine("Hwllo and Welcome to the Program");
+
+            Journal MyJournal = new Journal();
+            MyJournal.Run();
+        }
+    }
+}
