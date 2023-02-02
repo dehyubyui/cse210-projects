95162dee2cb77cdf55270f0bae40a67db9c71109
wdd130cassell <cas22015@byui.edu>
2023-02-02 02:17:38 +0000

    add

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,22 +1,107 @@
 using System;
 
-namespace JournalApp
-{
-
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello and Welcome to your Journal!");
-        Console.WriteLine("Please select one of the following");
-        Console.WriteLine("1. Add Not To Journal");
-        Console.WriteLine("2. Show Journal File");
-        Console.WriteLine("3.Load Journal Note");
-        Console.WriteLine("4.Save Journal Note");
-        Console.WriteLine("5.Quit Journal");
-        Console.Write("\r\nSelect an option:");
-    }
-}
+        Console.WriteLine("Welcome to your Journal");
+        Console.WriteLine("Please select an option listed below by the number listed");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        //you need to validate the user entered a number etc.  data scrub
+        int menuOption = Convert.ToInt32(Console.ReadLine());
 
+        //consider using a switch instead of if statement
+
+        switch(menuOption)
+        {
+            case 1:
+                //this is menu option 1
+                //ask a random question
+                Console.WriteLine("Please write an entry");
+                string foo = askQuestion();
+                Console.WriteLine(foo);
+                string entry = Console.ReadLine();
+                break;
+            case 2:
+                Console.WriteLine();
+                break;
+            case 3:
+            // load
+                break;
+            case 4:
+            //save
+                break;
+            case 5:
+            //quit
+                break;
+            default:
+                break;
+        }
+          //found an easier way to get these options selected
+            
+        if (menuOption == 1)
+        {
+            //Change to a function
+
+        }
+        else if (menuOption == 2)
+        {
+
+        }
+        if (menuOption == 2)
+        {
+            //change to function
+            Console.WriteLine("Display messages");
+        }
+
+        if (menuOption == 3)
+        {
+            Console.WriteLine("loading");
+        }
+
+        if (menuOption == 4)
+        {
+            Console.WriteLine("Save");
+        }
+
+        else
+        {
+            Console.WriteLine("quiting");
+        }
+        
+        
+        //asking a random question
+        static string askQuestion()
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,6);
+                string myQuestion = "";
+                switch (magicNumber)
+                {
+                    case 1:
+                    myQuestion = "Who was the most interesting person I interacted with today?";
+                    break;
+                    case 2:
+                    myQuestion = "What was the best part of my day?";
+                    break;
+                    case 3:
+                    myQuestion = "How did I see the hand of the Lord in my life today?";
+                    break;
+                    case 4:
+                    myQuestion = "What was the strongest emotion I felt today?";
+                    break;
+                    case 5:
+                    myQuestion = "If I had one thing I could do over today, what would it be?";
+                    break;
+
+
+                }
+            return myQuestion;
+            
+        }
+    }
 }
